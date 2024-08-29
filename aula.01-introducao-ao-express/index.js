901c0b411780314c4409b9a4cdf87ca5b62a0fb2
DSM2 <fatec-dsm2@academico.intra>
2024-08-29 19:30:26 -0300

    Aulas de diego

---
+++ aula.01-introducao-ao-express/index.js
@@ -0,0 +1,18 @@
+// Importando o Express na aplicação
+const express = require("express"); // CommonJS Modules
+// Criando uma instância do Express
+const app = express();
+
+// CRIANDO A ROTA PRINCIPAL
+app.get("/", function (req, res) {
+  res.send("Hello World! Bem-Vindo!");
+});
+
+// Iniciando o servidor na porta 8080
+app.listen(8080, function (error) {
+  if (error) {
+    console.log(`Ocorreu um erro: ${error}`);
+  } else {
+    console.log("Servidor iniciado com sucesso!");
+  }
+});
