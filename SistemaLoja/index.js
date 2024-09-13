71dfb5c494c5c16c7e16bcb6ade37c0b26736a74
Luiz <luizricardowm@outlook.com>
2024-09-13 18:02:37 -0300

    Iniciei o exercicio de DW2

--- SistemaLoja/index.js
+++ SistemaLoja/index.js
@@ -4,18 +4,30 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(express.static('public'));
-
-const port = 8080;
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
+
+app.get("/clientes", (req, res) => {
+  res.render("clientes.ejs");
+});
+
+app.get("/pedidos", (req,res) => {
+  res.render("pedidos.ejs");
+});
+
+app.get("/produtos", (req, res) => {
+  res.render("produtos.ejs");
+});
+
+const port = 8080;
 
 app.listen(port, (error) => {
   if (error) {
     console.log(`Ocorreu um erro: ${error}`);
-  }  else {
+  } else {
     console.log(`Servidor iniciado com sucesso em: http://localhost:${port}`);
   }
 });
