a409243742281547faace802e62e66aa8efee69c
Luiz <luizricardowm@outlook.com>
2024-09-27 02:23:05 -0300

    Loja feita

--- SistemaLoja/index.js
+++ SistemaLoja/index.js
@@ -1,25 +1,24 @@
-const express = require("express");
-
+// Importando o Express com ES6 Modules
+import express from "express";
+//Iniciando o Express na variável app
 const app = express();
 
+// Importando os Controllers (onde estão as rotas)
+import ClientesController from "./controllers/ClientesController.js";
+import PedidosController from "./controllers/PedidosController.js";
+import ProdutosController from "./controllers/ProdutosController.js";
+// Define o EJS como Renderizador de páginas
 app.set("view engine", "ejs");
-
+// Define o uso da pasta "public" para uso de arquivos estáticos
 app.use(express.static("public"));
+
+// Definindo o uso das rotas dos Controllers
+app.use("/", ClientesController);
+app.use("/", PedidosController);
+app.use("/", ProdutosController);
 
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/clientes", (req, res) => {
-  res.render("clientes.ejs");
-});
-
-app.get("/pedidos", (req,res) => {
-  res.render("pedidos.ejs");
-});
-
-app.get("/produtos", (req, res) => {
-  res.render("produtos.ejs");
 });
 
 const port = 8080;
