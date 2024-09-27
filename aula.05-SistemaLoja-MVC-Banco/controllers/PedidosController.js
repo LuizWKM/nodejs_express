cbb641a773ae5c3b3ccacbc9e9a1f4d4dc4f9d32
LuizWKM <luizricardowm@outlook.com>
2024-09-26 22:45:28 -0300

    Finalizando aula 05

--- aula.05-SistemaLoja-MVC-Banco/controllers/PedidosController.js
+++ aula.05-SistemaLoja-MVC-Banco/controllers/PedidosController.js
@@ -1,16 +1,50 @@
 import express from "express";
 const router = express.Router();
 // Importando o model de Pedido
-import Pedido from "../models/Pedido.js"
+import Pedido from "../models/Pedido.js";
 
 // ROTA PEDIDOS
 router.get("/pedidos", function (req, res) {
-  Pedido.findAll().then((pedidos) => {
-    res.render("pedidos", {
-      pedidos: pedidos,
+  Pedido.findAll()
+    .then((pedidos) => {
+      res.render("pedidos", {
+        pedidos: pedidos,
+      });
+    })
+    .catch((error) => {
+      console.log(erro);
     });
-  }).catch(error => {
-     console.log(erro)
-  })
 });
+
+// ROTA DE CADASTRO DE PEDIDOS
+router.post("/pedidos/new", (req, res) => {
+  const numero = req.body.numero;
+  const valor = req.body.valor;
+  Pedido.create({
+    numero: numero,
+    valor: valor,
+  })
+    .then(() => {
+      res.redirect("/pedidos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+// ROTA DE EXCLUSÃO DE PEDIDOS
+// ESSA ROTA POSSUI UM PARÂMETRO ID
+router.get("/pedidos/delete/:id", (req, res) => {
+  // COLETAR O ID QUE VEIO NA URL
+  const id = req.params.id
+  // MÉTODO PARA EXCLUIR
+  Pedido.destroy({
+    where:{
+      id: id
+    }
+  }).then(() => {
+    res.redirect("/pedidos")
+  }).catch(error => {
+    console.log(error)
+  })
+})
 export default router;
--- aula.05-SistemaLoja-MVC-Banco/controllers/ProdutosController.js
+++ aula.05-SistemaLoja-MVC-Banco/controllers/ProdutosController.js
@@ -1,17 +1,54 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
+// Importando o model de Produto
+import Produto from "../models/Produto.js";
 
 // ROTA PRODUTOS
-router.get("/produtos", function(req,res){
-    const produtos = [
-        {nome: "Celular Motorola E22", preco: 1200, categoria: "Eletroportáteis"},
-        {nome: "Tablet Samsung", preco: 900, categoria: "Eletrônicos"},
-        {nome: "Notebook Lenovo", preco: 3200, categoria: "Computadores"},
-        {nome: "Fone Bluetooth", preco: 150, categoria: "Periféricos"}
-    ]
-    res.render("produtos", {
-        produtos: produtos
+router.get("/produtos", (req, res) => {
+  Produto.findAll()
+    .then((produtos) => {
+      res.render("produtos", {
+        produtos: produtos,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE CADASTRO DE PRODUTOS
+router.post("/produtos/new", (req, res) => {
+  const nome = req.body.nome;
+  const preco = req.body.preco;
+  const categoria = req.body.categoria;
+
+  Produto.create({
+    nome: nome,
+    preco: preco,
+    categoria: categoria,
+  })
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+// ROTA DE EXCLUSÃO DE PRODUTOS
+// ESSA ROTA POSSUI UM PARÂMETRO ID
+router.get("/produtos/delete/:id", (req, res) => {
+    // COLETAR O ID QUE VEIO NA URL
+    const id = req.params.id
+    // MÉTODO PARA EXCLUIR
+    Produto.destroy({
+        where:{
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/produtos")
+    }).catch((error) => {
+        console.log(error)
     })
 })
 
-export default router
+export default router;
